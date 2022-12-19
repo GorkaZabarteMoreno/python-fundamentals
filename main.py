c3a7982038c4db83ca72c6e60e951543d6bef968
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2022-12-19 13:44:01 +0100

    Python packages and __init__.py file explanation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from packages.module1 import get_name
+from packages.module2 import get_height
+from packages import *
+
 if __name__ == '__main__':
     app_name: str = 'Gorka App'
     print('{0}'.format(app_name))
+
+    print(f'Name: {get_name()}')
+    print(f'Height: {get_height()}')
+    p = Point(5.0, 10.0)
+    print(p.x, p.y)
---
+++ packages/module2.py
@@ -0,0 +1,11 @@
+"""
+Module 2 of the package
+"""
+
+
+def get_weight() -> int:
+    return 75
+
+
+def get_height() -> float:
+    return 175.6
